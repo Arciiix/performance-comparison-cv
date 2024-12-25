9bf79c54dd968fae4207bbeb77657ee265441b00
Arciiix <artur.nowak100@gmail.com>
2024-12-25 12:24:46 +0100

    Add cpp file add gstreamer

---
+++ exampleCpp/exampleCpp.cpp
@@ -0,0 +1,44 @@
+﻿// exampleCpp.cpp : Defines the entry point for the application.
+//
+
+#include "exampleCpp.h"
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <gst/gst.h>
+
+int main(int arg, char* argv[])
+{
+    GstElement* pipeline = nullptr;
+    GstBus* bus = nullptr;
+    GstMessage* msg = nullptr;
+
+    std::cout << "Hello";
+    // gstreamer initialization
+    gst_init(&arg, &argv);
+
+    std::cout << "Init";
+
+
+    // building pipeline
+    pipeline = gst_parse_launch(
+        "playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
+        nullptr);
+
+    // start playing
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+    //wait until error or EOS ( End Of Stream )
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+        static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+
+    // free memory
+    if (msg != nullptr)
+        gst_message_unref(msg);
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+
+    return 0;
+}
